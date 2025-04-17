07818639fd92c5845dc31ddbf26d2f5c15b4c4dc
Emmanuel Arhu <108501930+emmanuelarhu@users.noreply.github.com>
2025-04-17 10:32:54 +0000

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(10));
+
+//        HBox topControls = createTopControls();
+//        VBox tableSection = createTableSection();
+//
+//        root.setTop(topControls);
+//        root.setCenter(tableSection);
+
+        Scene scene = new Scene(root, 900, 600);
+        primaryStage.setTitle("Employee Management System");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
